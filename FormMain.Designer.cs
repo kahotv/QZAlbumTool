222f85a13b2bac641c360240e003b5465782718e
Yui <root@kaho.tv>
2019-05-28 01:23:31 +0800

    初次提交

---
+++ FormMain.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace QZAlbumTool
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageLogin = new System.Windows.Forms.TabPage();
+            this.tabPageAlbum = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSelectNot = new System.Windows.Forms.Button();
+            this.btnSelectAll = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.web = new System.Windows.Forms.WebBrowser();
+            this.tabControl1.SuspendLayout();
+            this.tabPageLogin.SuspendLayout();
+            this.tabPageAlbum.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageLogin);
+            this.tabControl1.Controls.Add(this.tabPageAlbum);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(670, 442);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Selected += new System.Windows.Forms.TabControlEventHandler(this.TabControl1_Selected);
+            // 
+            // tabPageLogin
+            // 
+            this.tabPageLogin.Controls.Add(this.web);
+            this.tabPageLogin.Location = new System.Drawing.Point(4, 22);
+            this.tabPageLogin.Name = "tabPageLogin";
+            this.tabPageLogin.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageLogin.Size = new System.Drawing.Size(662, 416);
+            this.tabPageLogin.TabIndex = 0;
+            this.tabPageLogin.Text = "1.登录";
+            this.tabPageLogin.UseVisualStyleBackColor = true;
+            // 
+            // tabPageAlbum
+            // 
+            this.tabPageAlbum.Controls.Add(this.panel2);
+            this.tabPageAlbum.Controls.Add(this.panel1);
+            this.tabPageAlbum.Location = new System.Drawing.Point(4, 22);
+            this.tabPageAlbum.Name = "tabPageAlbum";
+            this.tabPageAlbum.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAlbum.Size = new System.Drawing.Size(662, 416);
+            this.tabPageAlbum.TabIndex = 1;
+            this.tabPageAlbum.Text = "2.相册";
+            this.tabPageAlbum.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Controls.Add(this.treeView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 46);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(656, 367);
+            this.panel2.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(88, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(568, 367);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(88, 367);
+            this.treeView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSelectNot);
+            this.panel1.Controls.Add(this.btnSelectAll);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(656, 43);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSelectNot
+            // 
+            this.btnSelectNot.Location = new System.Drawing.Point(557, 3);
+            this.btnSelectNot.Name = "btnSelectNot";
+            this.btnSelectNot.Size = new System.Drawing.Size(75, 34);
+            this.btnSelectNot.TabIndex = 1;
+            this.btnSelectNot.Text = "反选";
+            this.btnSelectNot.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectAll
+            // 
+            this.btnSelectAll.Location = new System.Drawing.Point(467, 3);
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(75, 34);
+            this.btnSelectAll.TabIndex = 0;
+            this.btnSelectAll.Text = "全选";
+            this.btnSelectAll.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Click += new System.EventHandler(this.BtnSelectAll_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(662, 416);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "3.关于";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // web
+            // 
+            this.web.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.web.Location = new System.Drawing.Point(3, 3);
+            this.web.MinimumSize = new System.Drawing.Size(20, 20);
+            this.web.Name = "web";
+            this.web.ScriptErrorsSuppressed = true;
+            this.web.Size = new System.Drawing.Size(656, 410);
+            this.web.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 442);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "FormMain";
+            this.Text = "QQ相册工具";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageLogin.ResumeLayout(false);
+            this.tabPageAlbum.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageLogin;
+        private System.Windows.Forms.TabPage tabPageAlbum;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnSelectNot;
+        private System.Windows.Forms.Button btnSelectAll;
+        private System.Windows.Forms.WebBrowser web;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace QZAlbumTool
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            web.Url = new Uri("https://qzone.qq.com");
+            
+        }
+
+        private string GetPSkey()
+        {
+            string p_skey = null;
+            try
+            {
+                var cookies = web.Document.Cookie.Split(';');
+                p_skey = cookies.Where(p => p.StartsWith(" p_skey=")).First().Split('=').ToArray()[1];
+            }
+            catch { }
+
+            return p_skey;
+        }
+        private string GetQQNumber()
+        {
+            string qqnumber = null;
+
+            try
+            {
+                var cookies = web.Document.Cookie.Split(';');
+                qqnumber = cookies.Where(p => p.StartsWith(" uin_cookie=")).First().Split('=').ToArray()[1];
+            }
+            catch { }
+
+            return qqnumber;
+        }
+        private int GetG_TK(string p_skey)
+        {
+            string e = p_skey;
+            var t = 5381;
+            for (int n = 0, r = e.Length; n < r; ++n)
+            {
+                t += (t << 5) + e[n];
+            }
+            return t & 0x7FFFFFFF;
+        }
+
+        private List<JObject> GetAlbumList(string p_skey,string qqnumber)
+        {
+            //检查登录 跳转到相册，失败就没登陆
+            List<JObject> listJAlbumList = null;
+
+            //string g_tk = WebBeginInvoke("return QZFL.pluginsDefine.getACSRFToken();");
+            string g_tk = GetG_TK(p_skey).ToString();
+            string url = "https://h5.qzone.qq.com/proxy/domain/photo.qzone.qq.com/fcgi-bin/fcg_list_album_v3?" +
+                "g_tk=" + g_tk +
+                "&callback=shine0_Callback" +
+                "&t=628680498" +
+                "&hostUin=" + qqnumber +
+                "&uin="+ qqnumber +
+                "&appid=4" +
+                "&inCharset=utf-8" +
+                "&outCharset=utf-8" +
+                "&source=qzone" +
+                "&plat=qzone" +
+                "&format=jsonp" +
+                "&notice=0" +
+                "&filter=1" +
+                "&handset=4" +
+                "&pageNumModeSort=40" +
+                "&pageNumModeClass=15" +
+                "&needUserInfo=1" +
+                "&idcNum=4" +
+                "&callbackFun=shine0" +
+                "&_=" + DateTime.UtcNow.ToFileTimeUtc().ToString();
+
+            WebClient wc = new WebClient() { Encoding = Encoding.UTF8 };
+            wc.Headers.Add("cookie", web.Document.Cookie);
+            wc.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+            string json = string.Empty;
+
+            try { json = wc.DownloadString(url); } catch { Thread.Sleep(1000); }
+            if(json == null) try { json = wc.DownloadString(url); } catch { }
+
+            if(json != null)
+            {
+                if (json.Contains("albumListModeClass"))
+                {
+                    // 相册列表
+                    int start = json.IndexOf("albumListModeClass");
+                    int end = json.LastIndexOf("}\r\n}\r\n);");
+                    if(end == -1)
+                        end = json.LastIndexOf("}\n}\n);");
+                    if (start >= 0 && end >= 0)
+                    {
+                        string jsAlbumListModeClass = "{\"" + json.Substring(start, end - start) + "}";
+
+                        JObject jobj = JObject.Parse(jsAlbumListModeClass);
+                        listJAlbumList = new List<JObject>();
+
+                        foreach (var jelem in jobj["albumListModeClass"])
+                        {
+                            foreach (var jelem2 in jelem["albumList"])
+                            {
+                                listJAlbumList.Add((JObject)jelem2);
+                            }
+                        }
+                        Debug.WriteLine("完成");
+                    }
+
+                    //显示在列表里
+                    /*
+                {
+                   "allowAccess" : 1,
+                   "anonymity" : 0,
+                   "bitmap" : "11000010",
+                   "classid" : 100,
+                   "comment" : 0,
+                   "createtime" : 123456,
+                   "desc" : "",
+                   "handset" : 0,
+                   "id" : "xxxxx",
+                   "lastuploadtime" : 123456,
+                   "modifytime" : 123456,
+                   "name" : "坦克大战",
+                   "order" : 1,
+                   "pre" : "http:\/\/b323.photo.store.qq.com\/psb?\/xxxxxx\/xxxxxxxx!\/a\/xxxxx",
+                   "priv" : 3,
+                   "pypriv" : 3,
+                   "total" : 6,
+                   "viewtype" : 0
+                },
+                    */
+                }
+            }
+
+            //获取相册列表 相册名称+封面缩略图
+            //获取相册内容列表  照片名称+缩略图+对应的原图
+            //下载原图
+
+            return listJAlbumList;
+        }
+        private Image GetAlbumFace(string pre,string name)
+        {
+            Image img = null;
+
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.Headers.Add("cookie", web.Document.Cookie);
+                //wc.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+                //string url = pre + "&rf=albumlist&t=5";
+                string url = pre + "&d=0";
+                if(url.Contains("/psb?/"))
+                {
+                    url = url.Replace("/a/", "/m/");
+                }
+                byte[] data = wc.DownloadData(url);
+                
+                using (MemoryStream mStream = new MemoryStream(data))
+                {
+                    img = Image.FromStream(mStream);
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+
+            return img;
+        }
+
+        private void TabControl1_Selected(object sender, TabControlEventArgs e)
+        {
+            if(e.TabPageIndex == 1)
+            {
+                string p_skey = GetPSkey();
+                string qq = GetQQNumber();
+                if(string.IsNullOrWhiteSpace(p_skey) || string.IsNullOrWhiteSpace(qq))
+                {
+                    MessageBox.Show("未登录，请重新登录");
+                    tabControl1.TabIndex = 0;
+                }
+                else
+                {
+                    var list = GetAlbumList(p_skey, qq);
+                    Debug.WriteLine("显示中,共" + list.Count + "个相册");
+                    ShowAlbumList(list);
+                }
+            }
+        }
+
+        private void ShowAlbumList(List<JObject> list)
+        {
+            listView1.Items.Clear();
+            listView1.LargeImageList = new ImageList();
+            listView1.LargeImageList.ImageSize = new Size(146, 110);
+            listView1.View = View.LargeIcon;
+            int i = 0;
+            foreach (var item in list)
+            {
+                dynamic jitem = item;
+                string name = jitem.name;
+                Image face = GetAlbumFace((string)jitem.pre, name);
+                //Debug.WriteLine("添加图片" + i + "| url:" + (string)jitem.pre);
+
+                if (face == null)
+                {
+                    Thread.Sleep(500);
+                    face = GetAlbumFace((string)jitem.pre, name);
+                }
+
+                if (face == null)
+                {
+                    Debug.WriteLine("添加图片" + i + "失败(" + (string)jitem.name + ")");
+                }
+                else
+                {
+                    listView1.LargeImageList.Images.Add(face);
+                    listView1.Items.Add(name, i++);
+                }
+
+
+            }
+        }
+
+        private void BtnSelectAll_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
